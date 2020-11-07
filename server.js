37ffa11fc190b47ed3a7d67bcb970b34010c70e3
Parada <paradafaii28@gmail.com>
2020-11-07 15:12:59 -0700

    create get request and test with postman, all work perfect

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
 
+const todos = [
+    {
+        message: "Wash Car...",
+        id:1
+    },
+    {
+        message: "Pay phone bill...",
+        id:2
+    },
+    {
+        message: "Prep food...",
+        id:3
+    }
+]
+
+app.get("/", (req,res) => {
+    res.status(200).json(todos);
+});
 
 const PORT = 5001;
 app.listen(PORT, () => {
